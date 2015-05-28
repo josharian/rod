e7e5ea7c3ec5f445e040eb0bfcc325f37223d324
Josh Bleecher Snyder <josharian@gmail.com>
2015-05-27 17:34:02 -0700

    now irrelevant

--- rod.go
+++ rod.go
@@ -1,182 +1,312 @@
 package main
 
 import (
-	"errors"
+	"bufio"
+	"encoding/json"
 	"flag"
-	"go/ast"
-	"go/build"
-	"go/parser"
-	"go/token"
+	"fmt"
+	"io"
 	"log"
 	"os"
-	"path/filepath"
+	"os/exec"
+	"regexp"
 	"strings"
-	"text/template"
-	"unicode"
-	"unicode/utf8"
+	"time"
+
+	"github.com/GaryBoone/GoStats/stats"
+	"golang.org/x/tools/benchmark/parse"
 )
 
-// benchmarks returns a list of all benchmark functions in the named files.
-func benchmarks(dir string, files []string) ([]string, error) {
-	var bb []string
-	fset := token.NewFileSet()
-	for _, file := range files {
-		path := filepath.Join(dir, file)
-		f, err := parser.ParseFile(fset, path, nil, 0)
+var (
+	debug  = flag.Bool("debug", false, "add debug output")
+	filter = flag.String("bench", ".", "regexp to filter benchmarks")
+)
+
+type benchmark struct {
+	name string
+	cmd  *exec.Cmd
+	in   io.Writer
+	out  *bufio.Reader
+}
+
+func newBenchmark(path string, args ...string) (*benchmark, error) {
+	var err error
+	var b benchmark
+	args = append([]string{"-test.run=NONE", "-test.benchserve"}, args...)
+	if *debug {
+		fmt.Println("RUN", path, strings.Join(args, " "))
+	}
+	b.cmd = exec.Command(path, args...)
+	if b.in, err = b.cmd.StdinPipe(); err != nil {
+		return nil, err
+	}
+	var stdout io.Reader
+	if stdout, err = b.cmd.StdoutPipe(); err != nil {
+		return nil, err
+	}
+	if *debug {
+		stdout = io.TeeReader(stdout, os.Stdout)
+	}
+	b.out = bufio.NewReader(stdout)
+	var stderr io.ReadCloser
+	stderr, err = b.cmd.StderrPipe()
+	if err != nil {
+		return nil, err
+	}
+	go func() {
+		r := bufio.NewReader(stderr)
+		s, err := r.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			log.Fatal(s)
+		}
+	}()
+	if err := b.cmd.Start(); err != nil {
+		return nil, err
+	}
+
+	// Consume any test output
+	var s string
+	for s != "PASS\n" {
+		s, err = b.out.ReadString('\n')
 		if err != nil {
 			return nil, err
 		}
-
-		// What name is "testing" imported under?
-		var tname string
-		for _, s := range f.Imports {
-			if s.Path.Value != `"testing"` {
-				continue
-			}
-			if tname != "" {
-				return nil, errors.New("testing imported twice")
-			}
-			if s.Name == nil {
-				tname = "testing"
-				continue
-			}
-			tname = s.Name.Name
-		}
-		if tname == "" {
-			continue // no benchmarks without testing.B
-		}
-
-		// Locate benchmarks.
-		for _, d := range f.Decls {
-			fn, ok := d.(*ast.FuncDecl)
-			if !ok {
-				continue
-			}
-			if isBenchmark(tname, fn) {
-				bb = append(bb, fn.Name.Name)
-			}
-		}
 	}
 
+	return &b, nil
+}
+
+func (b *benchmark) list() ([]string, error) {
+	if _, err := io.WriteString(b.in, "list\n"); err != nil {
+		return nil, err
+	}
+	// Ugly hack, but good enough
+	j, err := b.out.ReadBytes(']')
+	if err != nil {
+		return nil, err
+	}
+	// Consume the trailing \n
+	if _, err := b.out.ReadString('\n'); err != nil {
+		return nil, err
+	}
+	var bb []string
+	if err := json.Unmarshal(j, &bb); err != nil {
+		return nil, err
+	}
 	return bb, nil
 }
 
-// isBenchmark reports whether fn is a benchmark function declaration.
-// testing is the name that the testing package with imported under.
-func isBenchmark(testing string, fn *ast.FuncDecl) bool {
-	if !isBenchmarkName(fn.Name.String()) ||
-		fn.Recv != nil ||
-		fn.Type.Results != nil ||
-		fn.Type.Params == nil ||
-		len(fn.Type.Params.List) != 1 {
-		return false
+func (b *benchmark) run(benchmark string, length interface{}) (*parse.Benchmark, error) {
+	if *debug {
+		fmt.Printf("run %s %v\n", benchmark, length)
 	}
-
-	// Check that the type of the param is *testing.B.
-	star, ok := fn.Type.Params.List[0].Type.(*ast.StarExpr)
-	if !ok {
-		return false
+	if _, err := fmt.Fprintf(b.in, "run %s %v\n", benchmark, length); err != nil {
+		return nil, err
 	}
-	sel, ok := star.X.(*ast.SelectorExpr)
-	if !ok || sel.Sel.Name != "B" {
-		return false
+	line, err := b.out.ReadString('\n')
+	if err != nil {
+		return nil, err
 	}
-	id, ok := sel.X.(*ast.Ident)
-	return ok && id.Name == testing
+	return parse.ParseLine(line)
 }
 
-// isBenchmarkName reports whether name could be the name of a test function.
-func isBenchmarkName(name string) bool {
-	const prefix = "Benchmark"
-	if !strings.HasPrefix(name, prefix) {
-		return false
+func (b *benchmark) mustRun(benchmark string, length interface{}) *parse.Benchmark {
+	p, err := b.run(benchmark, length)
+	if err != nil {
+		log.Fatal(err)
 	}
-	if len(name) == len(prefix) { // Just "Benchmark" is ok
-		return true
-	}
-	r, _ := utf8.DecodeRuneInString(name[len(prefix):])
-	return !unicode.IsLower(r)
+	return p
 }
 
-func gen(pkg *build.Package, xtest bool, files []string) error {
-	bb, err := benchmarks(pkg.Dir, files)
+func (b *benchmark) itersFor(benchmark string, d time.Duration) int {
+	p, err := b.run(benchmark, d)
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
-	if len(bb) == 0 {
-		return nil
-	}
-
-	var suffix string
-	if xtest {
-		suffix = "_x"
-	}
-	f, err := os.Create(filepath.Join(pkg.Dir, "benchserv"+suffix+"_test.go"))
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	dot := struct {
-		Package    string
-		Benchmarks []string
-	}{
-		Package:    pkg.Name,
-		Benchmarks: bb,
-	}
-	if xtest {
-		dot.Package += "_test"
-	}
-
-	return tmpl.Execute(f, dot)
+	return p.N
 }
 
 func main() {
 	flag.Parse()
-	paths := flag.Args()
-	if len(paths) == 0 {
-		flag.Usage()
+	if flag.NArg() < 1 {
+		// TODO
+		fmt.Println("rod <compiled tests>")
 		os.Exit(2)
 	}
-	for _, path := range paths {
-		pkg, err := build.Import(path, ".", 0)
+
+	// Start all the tests.
+	// Find the intersection of benchmarks contained in all tests
+	var tests []*benchmark
+	allb := make(map[string]bool)
+	for i := 0; i < flag.NArg(); i++ {
+		// TODO: Run tests once, if requested, supporting regexp and short
+		t, err := newBenchmark(flag.Arg(i))
 		if err != nil {
 			log.Fatal(err)
 		}
-		if err := gen(pkg, false, pkg.TestGoFiles); err != nil {
+		t.name = flag.Arg(i)
+		tests = append(tests, t)
+		bb, err := t.list()
+		if err != nil {
 			log.Fatal(err)
 		}
-		if err := gen(pkg, true, pkg.XTestGoFiles); err != nil {
-			log.Fatal(err)
+		if i == 0 {
+			for _, b := range bb {
+				allb[b] = true
+			}
+			continue
+		}
+		for e := range allb {
+			var found bool
+			for _, b := range bb {
+				if e == b {
+					found = true
+					break
+				}
+			}
+			if !found {
+				delete(allb, e)
+			}
+		}
+	}
+
+	re, err := regexp.Compile(*filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var matched []string
+	for b := range allb {
+		if re.MatchString(b) {
+			matched = append(matched, b)
+		}
+	}
+
+	for _, b := range matched {
+		for _, t := range tests {
+			var sts [2]stats.Stats
+			h := t.itersFor(b, 1000*time.Millisecond)
+			if h < 2 {
+				h = 2
+			}
+			for x := 1; x <= 2; x++ {
+				st := &sts[x-1]
+				fmt.Println(t.name, b, h)
+				for j := 0; j < 100; j++ {
+					var p *parse.Benchmark
+					if x == 1 {
+						p = t.mustRun(b, x)
+					} else {
+						p = t.mustRun(b, h)
+					}
+					st.Update(p.NsPerOp * float64(p.N))
+					// fmt.Println(x, p.NsPerOp)
+				}
+				fmt.Printf("n=%d\tcount=%d\tmean=%f\tvar=%f\tskew=%f\tkurt=%f\n",
+					x, st.Count(),
+					st.Mean(), st.SampleVariance(),
+					st.SampleSkew(), st.SampleKurtosis(),
+				)
+			}
+			// fmt.Printf("delta:\tmean=%f\tvar=%f\n",
+			// 	sts[1].Mean()-sts[0].Mean(), sts[1].SampleVariance()-sts[0].SampleVariance(),
+			// )
+			codemean := (sts[1].Mean() - sts[0].Mean()) / float64(h-1)
+			overheadmean := sts[0].Mean() - codemean
+			// codevar := sts[1].SampleVariance() - sts[0].SampleVariance()
+			// overheadvar := sts[0].SampleVariance() - codevar
+			// fmt.Printf("code mean=%f var=%f overhead mean=%f var=%f\n", codemean, codevar, overheadmean, overheadvar)
+			fmt.Printf("code mean=%f overhead mean=%f\n", codemean, overheadmean)
+
+			// Goal is that the mean should be dominated by the code time, not the overhead.
+			// Therefore we want to pick N such that overheadmean = 1% * N * codemean,
+			// where 1% is arbitrary. So N = overheadmean / (codemean * target).
+			target := 0.01
+			n := overheadmean / (codemean * target)
+			ndur := time.Duration(n * codemean)
+			fmt.Println("TARGET N", int(n), "DUR", ndur)
+
+			// p0 := t.mustRun(b, 3*time.Second)
+			// var st stats.Stats
+			// var reg stats.Regression
+			// for j := 0; j < 200; j++ {
+			// 	n := rand.Intn(p0.N) + 1
+			// 	p := t.mustRun(b, n)
+			// 	// reg.Update(float64(p.N), p.NsPerOp)
+			// 	// st.Update(p.NsPerOp)
+			// 	fmt.Printf("%f\t%f\n", float64(p.N), p.NsPerOp*float64(p.N))
+			// 	// fmt.Printf("%v %v mean=%f skew=%f kurtosis=%f\n",
+			// 	// 	t.name, b,
+			// 	// 	st.Mean(),
+			// 	// 	st.SampleSkew(), st.SampleKurtosis())
+			// }
+			// _ = st
+			// _ = reg
+			// fmt.Printf("%v count=%v r2=%.2f slope=%.2f slopeerr=%.2f intercept=%.2f intercepterr=%.2f\n",
+			// 	b,
+			// 	reg.Count(), reg.RSquared(),
+			// 	reg.Slope(), reg.SlopeStandardError(),
+			// 	reg.Intercept(), reg.InterceptStandardError(),
+			// )
+
+			// 	continue
+			// 	n := 1
+			// 	avg := func() float64 {
+			// 		const N = 3
+			// 		var sum float64
+			// 		for j := 0; j < N; j++ {
+			// 			p := before.mustRun(b, n)
+			// 			sum += p.NsPerOp
+			// 		}
+			// 		sum /= N
+			// 		return sum
+			// 	}
+			// 	prev := avg()
+			// 	for {
+			// 		n <<= 1
+			// 		now := avg()
+			// 		if now >= prev {
+			// 			break
+			// 		}
+			// 		prev = now
+			// 	}
+			// 	fmt.Printf("%v\t%d: %v\n", b, n, int64(prev))
+
+			// 	continue
+			// 	// In decreasing time order
+			// 	durations := []time.Duration{
+			// 		100 * time.Millisecond,
+			// 		1 * time.Millisecond,
+			// 		10 * time.Microsecond,
+			// 	}
+			// 	nn := make([]int, len(durations))
+			// 	for i := range nn {
+			// 		nn[i] = 1
+			// 	}
+			// 	for i, d := range durations {
+			// 		nn[i] = before.itersFor(b, d)
+			// 		// We only got one iteration in during this
+			// 		// long duration, so we'll only get one is
+			// 		// during the shorter ones as well.
+			// 		if nn[i] == 1 {
+			// 			break
+			// 		}
+			// 	}
+			// 	const iters = 50
+			// 	for i := range nn {
+			// 		var st stats.Stats
+			// 		for iter := 0; iter < iters; iter++ {
+			// 			p, err := before.run(b, nn[i])
+			// 			if err != nil {
+			// 				log.Fatal(err)
+			// 			}
+			// 			st.Update(p.NsPerOp)
+			// 		}
+			// 		fmt.Printf("%s\t(%v => %d)\tn=%d\tmean=%f\tvar=%f\n",
+			// 			b, durations[i], nn[i], iters, st.Mean(), st.SampleVariance())
+			// 	}
+			// }
 		}
 	}
 }
-
-// Man, I sure do wish issue 9969 would get fixed.
-var (
-	raw = `\
-// Autogenerated by github.com/josharian/rod
-
-package {{.Package}}
-
-import (
-	"testing"
-
-	"github.com/josharian/rod/server"
-)
-
-func TestStartRodServer(*testing.T) {
-	server.Start()
-}
-
-func init() {
-	server.Register([]server.Benchmark{
-{{range .Benchmarks}}\
-		{Name: "{{.}}", F: {{.}}},
-{{end}}\
-	})
-}
-`
-	nonewline = strings.Replace(raw, "\\\n", "", -1)
-	tmpl      = template.Must(template.New("benchserve").Parse(nonewline))
-)
