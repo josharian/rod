1eed2f475d37aeb58b8f28abba7a552d24ce6ef8
Josh Bleecher Snyder <josharian@gmail.com>
2015-05-27 17:43:02 -0700

    update to new benchserve, breaking things along the way

--- rod.go
+++ rod.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
@@ -66,15 +65,6 @@ func newBenchmark(path string, args ...string) (*benchmark, error) {
 		return nil, err
 	}
 
-	// Consume any test output
-	var s string
-	for s != "PASS\n" {
-		s, err = b.out.ReadString('\n')
-		if err != nil {
-			return nil, err
-		}
-	}
-
 	return &b, nil
 }
 
@@ -82,27 +72,25 @@ func (b *benchmark) list() ([]string, error) {
 	if _, err := io.WriteString(b.in, "list\n"); err != nil {
 		return nil, err
 	}
-	// Ugly hack, but good enough
-	j, err := b.out.ReadBytes(']')
-	if err != nil {
-		return nil, err
-	}
-	// Consume the trailing \n
-	if _, err := b.out.ReadString('\n'); err != nil {
-		return nil, err
-	}
 	var bb []string
-	if err := json.Unmarshal(j, &bb); err != nil {
-		return nil, err
+	for {
+		line, err := b.out.ReadString('\n')
+		if err != nil {
+			return nil, err
+		}
+		if line == "\n" {
+			break
+		}
+		bb = append(bb, line[:len(line)-1])
 	}
 	return bb, nil
 }
 
-func (b *benchmark) run(benchmark string, length interface{}) (*parse.Benchmark, error) {
+func (b *benchmark) run(benchmark string, n int) (*parse.Benchmark, error) {
 	if *debug {
-		fmt.Printf("run %s %v\n", benchmark, length)
+		fmt.Printf("run %s %d\n", benchmark, n)
 	}
-	if _, err := fmt.Fprintf(b.in, "run %s %v\n", benchmark, length); err != nil {
+	if _, err := fmt.Fprintf(b.in, "run %s %d\n", benchmark, n); err != nil {
 		return nil, err
 	}
 	line, err := b.out.ReadString('\n')
@@ -112,20 +100,12 @@ func (b *benchmark) run(benchmark string, length interface{}) (*parse.Benchmark,
 	return parse.ParseLine(line)
 }
 
-func (b *benchmark) mustRun(benchmark string, length interface{}) *parse.Benchmark {
-	p, err := b.run(benchmark, length)
+func (b *benchmark) mustRun(benchmark string, n int) *parse.Benchmark {
+	p, err := b.run(benchmark, n)
 	if err != nil {
 		log.Fatal(err)
 	}
 	return p
-}
-
-func (b *benchmark) itersFor(benchmark string, d time.Duration) int {
-	p, err := b.run(benchmark, d)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return p.N
 }
 
 func main() {
@@ -186,10 +166,7 @@ func main() {
 	for _, b := range matched {
 		for _, t := range tests {
 			var sts [2]stats.Stats
-			h := t.itersFor(b, 1000*time.Millisecond)
-			if h < 2 {
-				h = 2
-			}
+			h := 2
 			for x := 1; x <= 2; x++ {
 				st := &sts[x-1]
 				fmt.Println(t.name, b, h)
